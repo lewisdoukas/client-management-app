30592abc9b43c272013a15f2c3c438ad8974771d
Ilias Doukas <doukas.lewis@gmail.com>
2024-09-20 20:05:13 +0300

    Add github provider

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,9 @@ export default {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    GitHub,
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
   ],
 } satisfies NextAuthConfig;
